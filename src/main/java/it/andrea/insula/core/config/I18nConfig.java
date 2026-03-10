f7e30b533ae5f44207e29c217f6ab2fe1538befa
Andrea Donadello <andreadonadello@protonmail.com>
2026-03-10 22:24:00 +0100

    Remove locale filter

--- src/main/java/it/andrea/insula/core/config/I18nConfig.java
+++ src/main/java/it/andrea/insula/core/config/I18nConfig.java
@@ -1,12 +1,8 @@
 package it.andrea.insula.core.config;
 
-import it.andrea.insula.core.filter.LocaleFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.core.Ordered;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
@@ -23,18 +19,7 @@ public class I18nConfig {
         resolver.setDefaultLocale(Locale.ITALIAN);
         resolver.setSupportedLocales(List.of(Locale.ITALIAN, Locale.ENGLISH));
 
-        LocaleContextHolder.setDefaultLocale(Locale.ITALIAN);
-
         return resolver;
-    }
-
-    @Bean
-    public FilterRegistrationBean<LocaleFilter> localeFilterRegistration(LocaleResolver localeResolver) {
-        FilterRegistrationBean<LocaleFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new LocaleFilter(localeResolver));
-        registration.addUrlPatterns("/*");
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registration;
     }
 
     @Bean
--- src/main/java/it/andrea/insula/core/filter/package-info.java
+++
@@ -1,3 +0,0 @@
-@org.springframework.modulith.NamedInterface("shared-filter")
-package it.andrea.insula.core.filter;
-
