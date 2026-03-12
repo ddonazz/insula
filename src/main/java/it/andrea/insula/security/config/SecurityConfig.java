33f5e51d177d5292d0b565be94c8568f5bdcb1ee
Andrea Donadello <andreadonadello@protonmail.com>
2026-03-12 21:20:54 +0100

    Update CORS configuration to allow all origins for improved API accessibility

--- src/main/java/it/andrea/insula/security/config/SecurityConfig.java
+++ src/main/java/it/andrea/insula/security/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept-Language"));
         configuration.setAllowCredentials(true);
