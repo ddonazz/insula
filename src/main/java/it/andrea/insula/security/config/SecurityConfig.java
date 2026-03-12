aac6bc60ccacfe849b32f3b3f74f9354981350ec
Andrea Donadello <andreadonadello@protonmail.com>
2026-03-12 21:27:57 +0100

    Update CORS configuration to use allowedOriginPatterns for improved security

--- src/main/java/it/andrea/insula/security/config/SecurityConfig.java
+++ src/main/java/it/andrea/insula/security/config/SecurityConfig.java
@@ -69,10 +69,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept-Language"));
         configuration.setAllowCredentials(true);
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
